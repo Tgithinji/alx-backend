d825f961ea3355ac088da4a141c34d0352881df4
Tgithinji <t.muraguri420@gmail.com>
2024-04-09 22:10:40 +0300

    Basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basic flask app
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """render index page
+    """
+    return render_template('0-index.html')
